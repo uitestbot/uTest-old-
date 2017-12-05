99e428ca1d1d469d46ef85996802c3447c8b1462
uitestbot <uitestbot@gmail.com>
2017-12-05 23:48:43 +0200

    Added metro dark theme to the ui-test-bot project. Integrated and modified metro dark theme to the ui-test-bot project. Added auto increment extension support for visual studio. Added basic Dashboard window to the ui-test-bot project. Updated post-build events. Restructured the ui-test-bot project.

---
+++ ui-test-bot/ui-test-bot/Dashboard/Dashboard.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ui_test_bot
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class Dashboard : Window
+    {
+        public Dashboard()
+        {
+            InitializeComponent();
+            versionLabel.Content = Assembly.GetExecutingAssembly().GetName().Version.ToString();
+        }
+
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.IsEnabled = false;
+            ExitConfirmation exitConfirmation = new ExitConfirmation();
+            exitConfirmation.OwningWindow = this;
+            exitConfirmation.Show();
+        }
+
+        private void MinimizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+    }
+}
---
+++ ui-test-bot/ui-test-bot/Dashboard/ExitConfirmation.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ui_test_bot
+{
+    /// <summary>
+    /// Interaction logic for ExitConfirmation.xaml
+    /// </summary>
+    public partial class ExitConfirmation : Window
+    {
+        public Dashboard OwningWindow { get; set; }
+
+        public ExitConfirmation()
+        {
+            InitializeComponent();
+        }
+
+        private void YesButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+            if (OwningWindow != null)
+                OwningWindow.Close();
+        }
+
+        private void NoButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+            OwningWindow.IsEnabled = true;
+        }
+
+        private void ExitConfirm_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Y)
+            {
+                this.Close();
+                if (OwningWindow != null)
+                    OwningWindow.Close();
+            }
+            else if (e.Key == Key.N)
+            {
+                OwningWindow.IsEnabled = true;
+                this.Close();
+            }
+        }
+
+    }
+}
